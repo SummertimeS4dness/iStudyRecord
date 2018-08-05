93f1f6755fc496f63a4e92ee4d0480902f0054da
yehormonko <monkoyehor@gmail.com>
2018-08-05 14:39:37 +0300

    part of logging and exception handling

---
+++ war/src/mvc/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package mvc.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+@ExceptionHandler(Exception.class)
+public void handleIOException(Exception ex) throws Exception {
+		//logger here
+}
+}
